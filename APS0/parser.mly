ea6ef8c7d50864e4cbfaf6745d5870dc67dc1909
Abdelkader Boumessaoud <abdelkader.boumessaoud.pro@gmail.com>
2024-03-22 15:16:37 +0100

    aps0 to finish

--- APS0/parser.mly
+++ APS0/parser.mly
@@ -1,92 +1,79 @@
 %{
-(* ========================================================================== *)
-(* == UPMC/master/info/4I506 -- Janvier 2016/2017                          == *)
-(* == SU/FSI/master/info/MU4IN503 -- Janvier 2020/2021/2022                == *)
-(* == Analyse des programmes et sémantiques                                == *)
-(* ========================================================================== *)
-(* == hello-APS Syntaxe ML                                                 == *)
-(* == Fichier: parser.mly                                                  == *)
-(* == Analyse syntaxique                                                   == *)
-(* ========================================================================== *)
-
 open Ast
-
 %}
-  
-%token <int> NUM
-%token <string> IDENT
+
+
 %token LBRA RBRA
-%token LPAR RPAR 
-%token CONST FUN REC IF ECHO
-%token SEMCOL COL COMA ARROW
-%token STAR
+%token LPAR RPAR
+%token SEMCOL COL COMA ARROW STAR
+%token CONST FUN REC ECHO IF
 %token AND OR
 %token BOOL INT
+%token <int> NUM
+%token <string> IDENT
 
 %type <Ast.cmds> prog
 %type <Ast.cmds> cmds
 %type <Ast.def> def
-%type <Ast.stat> stat
-%type <Ast.expr> expr
-%type <Ast.expr list> exprs
 %type <Ast.singleType> singleType
-%type <Ast.types> types
-%type <Ast.arg> arg
-%type <Ast.args> args
+%type <Ast.singleType list> types
+%type <Ast.singleArg> singleArg
+%type <Ast.singleArg list> args
+%type <Ast.stat> stat
+%type <Ast.singleExpr> singleExpr
+%type <Ast.singleExpr list> exprs
 
 %start prog
 
 %%
-prog: LBRA cmds RBRA    { $2 }
+prog: 
+  | LBRA cmds RBRA { $2 }
 ;
 
 cmds:
-  stat                  { ASTStat $1 }
-  | def SEMCOL cmds     { ASTDef($1, $3) }
+  | stat { ASTStat($1) }
+  | def SEMCOL cmds { ASTDef($1, $3) }
 ;
 
 def:
-  CONST IDENT singleType expr {ASTConst($2, $3, $4)}
-  | FUN IDENT singleType LBRA args RBRA expr  {ASTFunct($2, $3, $5, $7)}
-  | FUN REC IDENT singleType LBRA args RBRA expr {ASTRecFunct($3, $4, $6, $8)} 
-;
-
-
-stat:
-  ECHO expr             { ASTEcho($2) }
-;
-
-expr:
-  NUM                   { ASTNum($1) }
-| IDENT                 { ASTId($1) }
-| LPAR IF expr expr expr RPAR { ASTIf($3, $4, $5)}
-| LPAR AND expr expr RPAR {ASTAnd($3, $4)}
-| LPAR OR expr expr RPAR  {ASTOr($3, $4)}
-| LPAR expr exprs RPAR  { ASTApp($2, $3) }
-| LBRA args RBRA expr {ASTLambdaExpression($2, $4)}
-;
-
-exprs :
-  expr       { [$1] }
-| expr exprs { $1::$2 }
+  | CONST IDENT singleType singleExpr { ASTConst($2, $3, $4) }
+  | FUN IDENT singleType LBRA args RBRA singleExpr  { ASTFun($2, $3, $5, $7) }
+  | FUN REC IDENT singleType LBRA args RBRA singleExpr { ASTFunRec($3, $4, $6, $8) } 
 ;
 
 singleType:
-  INT       { Type(Int) }
-  | BOOL    { Type(Bool) }  
-  | LPAR types ARROW singleType RPAR {TypeFunc($2, $4)}
+  | INT { Type(Int) }
+  | BOOL { Type(Bool) }  
+  | LPAR types ARROW singleType RPAR { TypeFun($2, $4) }
 ;
-
 types:
-  singleType { ASTType($1)}
-| singleType STAR types { ASTTypes($1, $3)}
+  | singleType { [$1] }
+  | singleType STAR types { $1::$3 }
 ;
 
-arg: IDENT COL singleType {ASTArg($1, $3)}
+singleArg: 
+  | IDENT COL singleType { ASTSingleArg($1, $3) }
 ;
-
 args:
-  arg {ASTOneArg($1)}
-| arg COMA args {ASTArgs($1, $3)}
+  | singleArg { [$1] }
+  | singleArg COMA args { $1::$3 }
 ;
 
+stat:
+  | ECHO singleExpr { ASTEcho($2) }
+;
+
+singleExpr:
+  | NUM { ASTNum($1) }
+  | IDENT { ASTId($1) }
+  | LPAR IF singleExpr singleExpr singleExpr RPAR { ASTIf($3, $4, $5) }
+  | LPAR AND singleExpr singleExpr RPAR { ASTAnd($3, $4) }
+  | LPAR OR singleExpr singleExpr RPAR { ASTOr($3, $4) }
+  | LPAR singleExpr exprs RPAR { ASTApp($2, $3) }
+  | LBRA args RBRA singleExpr { ASTLambdaExpression($2, $4) }
+;
+
+exprs :
+  | singleExpr       { [$1] }
+  | singleExpr exprs { $1::$2 }
+;
--- APS0/prologTerm.ml
+++ APS0/prologTerm.ml
@@ -1,175 +1,181 @@
-(* ========================================================================== *)
-(* == UPMC/master/info/4I506 -- Janvier 2016/2017/2018                     == *)
-(* == SU/FSI/master/info/MU4IN503 -- Janvier 2020/2021/2022                == *)
-(* == Analyse des programmes et sémantiques                                == *)
-(* ========================================================================== *)
-(* == hello-APS Syntaxe ML                                                 == *)
-(* == Fichier: prologTerm.ml                                               == *)
-(* ==  Génération de termes Prolog                                         == *)
-(* ========================================================================== *)
 open Ast
-  
-let stringify t =
-  match t with 
-    | Int -> "int"
-    | Bool -> "bool"
-
 
 let rec print_singleType stype =
-  match stype with 
-    | Type(t)-> Printf.printf " %s " (stringify t)
-    | TypeFunc(types, t) -> (
-      Printf.printf "FuncType(";
-      print_types types;
-      Printf.printf " -> ";
-      print_singleType t;
-      Printf.printf ")"
-    )
+  match stype with
+  | Type(Bool)-> Printf.printf "bool"
+  | Type(Int)-> Printf.printf "int"
+  | TypeFun(types, t) -> (
+    Printf.printf "funType";
+    Printf.printf "(";
+    Printf.printf "[";
+    print_types types;
+    Printf.printf "]";
+    Printf.printf ",";
+    print_singleType t;
+    Printf.printf ")")
+and print_types ts =
+  match ts with
+  | [] -> ()
+  | [a] -> print_singleType a
+  | h::tail -> (
+    print_singleType h;
+    Printf.printf "*";
+    print_types tail)
 
-  and print_types ts = 
-    match ts with 
-        ASTType(t) -> print_singleType t
-      | ASTTypes(t, types) -> (
-          print_singleType t;
-          Printf.printf " * "; 
-          print_types types;
-        )
-      
-let print_arg arg =
-  match arg with
-    ASTArg (ident, t) -> (
-      Printf.printf "Arg(" ;
-      Printf.printf "%s" ident ;
-      Printf.printf ":";
-      print_singleType t;
-      Printf.printf ")"
-      )
+let print_singleArg sarg =
+  match sarg with
+  | ASTSingleArg (ident, t) -> (
+    Printf.printf "singleArg(" ;
+    Printf.printf "%s" ident ;
+    Printf.printf ":";
+    print_singleType t;
+    Printf.printf ")")
 
 let rec print_args args =
   match args with
-      ASTOneArg a -> print_arg a
-    | ASTArgs(a, argss) -> (
-      print_arg a;
-      Printf.printf "," ;
-      print_args argss
-    )
+  | [] -> ()
+  | [a] -> print_singleArg a
+  | h::tail ->
+      print_singleArg h;
+      Printf.printf ",";
+      print_args tail
 
-
-let rec print_expr e =
+let rec print_singleExpr e =
   match e with
-      ASTNum n -> Printf.printf"num(%d)" n
-    | ASTId x -> Printf.printf"id(%s)" x
-    | ASTApp(e, es) -> (
-        Printf.printf"app(";
-        print_expr e;
-        Printf.printf",[";
-        print_exprs es;
-        Printf.printf"])"
-      )
-    | ASTIf(cond, consequence, alternative) -> (
-        Printf.printf"if ";
-        print_expr cond;
-        Printf.printf", consequence ";
-        print_expr consequence;
-        Printf.printf", alternative ";
-        print_expr alternative;
-      )
-    | ASTAnd(op1, op2) -> (
-        Printf.printf"and ";
-        Printf.printf"operand 1 ";
-        print_expr op1;
-        Printf.printf"operand 2 ";
-        print_expr op2;
-      )
-    | ASTOr(op1, op2) -> (
-        Printf.printf"or ";
-        Printf.printf"operand 1 ";
-        print_expr op1;
-        Printf.printf"operand 2 ";
-        print_expr op2;
-      )
-    | ASTLambdaExpression(args, expr) -> (
-        Printf.printf "Lambda ([";
-        print_args args;
-        Printf.printf "],";
-        Printf.printf " init(";
-        print_expr expr;
-        Printf.printf ")";
-        Printf.printf ")"
-    )
+  | ASTNum n -> Printf.printf "num(%d)" n
+  | ASTId x -> Printf.printf "id(%s)" x
+  | ASTIf(cond, consequence, alternative) -> (
+      Printf.printf "if";
+      Printf.printf "(";
+      print_singleExpr cond;
+      Printf.printf ",";
+      print_singleExpr consequence;
+      Printf.printf ",";
+      print_singleExpr alternative;
+      Printf.printf ")";)
+  | ASTAnd(op1, op2) -> (
+      Printf.printf "and";
+      Printf.printf "(";
+      print_singleExpr op1;
+      Printf.printf ",";
+      print_singleExpr op2;
+      Printf.printf ")";)
+  | ASTOr(op1, op2) -> (
+      Printf.printf "or";
+      Printf.printf "(";
+      print_singleExpr op1;
+      Printf.printf ",";
+      print_singleExpr op2;
+      Printf.printf ")";)
+  | ASTApp(e, es) -> (
+      Printf.printf "app(";
+      print_singleExpr e;
+      Printf.printf ",(";
+      print_exprs es;
+      Printf.printf "))")
+  | ASTLambdaExpression(args, singleExpr) -> (
+      Printf.printf "lambda";
+      Printf.printf "(";
+      Printf.printf "[";
+      print_args args;
+      Printf.printf "]";
+      Printf.printf ",";
+      print_singleExpr singleExpr;
+      Printf.printf ")";)
 
 and print_exprs es =
   match es with
-      [] -> ()
-    | [e] -> print_expr e
-    | e::es -> (
-	print_expr e;
-	print_char ',';
-	print_exprs es
-      )
+  | [] -> ()
+  | [e] ->
+      print_singleExpr e
+  | e::es -> (
+      print_singleExpr e;
+      print_char ',';
+      print_exprs es)
 
 let print_stat s =
   match s with
-      ASTEcho e -> (
-	Printf.printf("echo(");
-	print_expr(e);
-	Printf.printf(")")
-      )
+  | ASTEcho e -> (
+      Printf.printf "echo";
+      Printf.printf "(";
+      print_singleExpr e;
+      Printf.printf ")")
 
 let print_def d =
   match d with
-      ASTConst(id, t, e) -> (
-        Printf.printf "CONST( ";
-        Printf.printf "%s," id;
-        print_singleType t; 
-        Printf.printf ",";
-        print_expr e;
-        Printf.printf ")";
-      )
-    | ASTFunct(id, t, args, e) -> (
-        Printf.printf "Funtion(";
-        Printf.printf "%s," id;
-        print_singleType t;
-        Printf.printf "[";
-        print_args args;
-        Printf.printf "]";
-        print_expr e;
-        Printf.printf ")"
-    )
-    | ASTRecFunct(id, t, args, e) -> (
-        Printf.printf "RecFuntion(";
-        Printf.printf "rec ";
-        Printf.printf "%s," id;
-        print_singleType t;
-        Printf.printf "[";
-        print_args args;
-        Printf.printf "]";
-        print_expr e;
-        Printf.printf ")"
-    )
+  | ASTConst(id, t, e) -> (
+      Printf.printf "const";
+      Printf.printf "(";
+      Printf.printf "%s," id;
+      print_singleType t;
+      Printf.printf ",";
+      print_singleExpr e;
+      Printf.printf ")";)
+  | ASTFun(id, t, args, e) -> (
+      Printf.printf "fun";
+      Printf.printf "(";
+      Printf.printf "%s," id;
+      Printf.printf ",";
+      print_singleType t;
+      Printf.printf ",";
+      Printf.printf "[";
+      print_args args;
+      Printf.printf "]";
+      Printf.printf ",";
+      print_singleExpr e;
+      Printf.printf ")")
+  | ASTFunRec(id, t, args, e) -> (
+      Printf.printf "funRec";
+      Printf.printf "(";
+      Printf.printf "%s," id;
+      Printf.printf ",";
+      print_singleType t;
+      Printf.printf ",";
+      Printf.printf "[";
+      print_args args;
+      Printf.printf "]";
+      Printf.printf ",";
+      print_singleExpr e;
+      Printf.printf ")")
 
 let rec print_cmds c =
   match c with
-      ASTStat s -> print_stat s
-    | ASTDef(def,c) -> (
-      print_def def; 
-      Printf.printf " : ";
-      print_cmds c
-    )
-	
+  | ASTStat s -> print_stat s
+  | ASTDef(def,c) -> (
+    Printf.printf "cmds";
+    Printf.printf "(";
+    print_def def;
+    Printf.printf ";";
+    print_cmds c;
+    Printf.printf ")")
+
 let print_prog p =
-  Printf.printf("prog([");
+  Printf.printf("prog");
+  Printf.printf("(");
+  Printf.printf("[");
   print_cmds p;
-  Printf.printf("])")
+  Printf.printf("]");
+  Printf.printf(")");
+  Printf.printf(".\n")
 ;;
-	
-let fname = Sys.argv.(1) in
+
+let fname = Sys.argv.(1) in (*debugger fait avec l'aide du pro, merci a lui*)
 let ic = open_in fname in
-  try
-    let lexbuf = Lexing.from_channel ic in
-    let p = Parser.prog Lexer.token lexbuf in
-      print_prog p;
-      print_string ".\n"
-  with Lexer.Eof ->
-    exit 0
-      
+try
+  let lexbuf = Lexing.from_channel ic in
+  let p = 
+    try
+      Parser.prog Lexer.token lexbuf
+    with _ ->
+      let open Lexing in 
+      let curr = lexbuf.lex_curr_p in 
+      let line = curr.pos_lnum in
+      let cnum = curr.pos_cnum - curr.pos_bol in
+      Printf.printf "Erreur de syntaxe à la ligne %d, colonne %d\n" line cnum;
+      exit 1
+  in
+
+  print_prog p;
+  print_string ".\n"
+with Lexer.Eof ->
+  exit 0
